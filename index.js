586850f9e3b95fa223af41ed34b160bfec6cd3c5
Pablo Villacanas <pablo.villacanas@gmail.com>
2019-11-25 18:50:28 +0100

    Exports functions of module

--- index.js
+++ index.js
@@ -4,5 +4,9 @@
  * 
  * @param {Object} jsonObject Object on which it will combinations will be generated  
  */
-const generator = require("src/combinator/object_generator.js").generator;
-const attr_analyzer = require("src/exporters/attribute_analyzer.js").attr_analyzer;
+const generator = require("./src/combinator/object_generator.js").generator;
+
+const attr_analyzer = require("./src/exporters/attribute_analyzer.js").attr_analyzer;
+
+exports.generator = generator;
+exports.attr_analyzer = attr_analyzer;
